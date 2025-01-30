20bb866e5d1b0ea4307a30bed1fc77abd86bb05d
hyunjoonKim92 <hyunjoon1992@naver.com>
2025-01-30 18:21:26 +0900

    -- 250130 commit - react + ts migration - MUI Sign In Template 도입

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import House from './layout/House';
+
+class App extends React.Component {
+  render() {
+    return (
+      <House />
+    );
+  }
+}
+
+export default App;
--- src/layout/House.js
+++ src/layout/House.js
@@ -1,15 +1,14 @@
 import React from 'react';
-
 import Header from './Header';
 import Map from './Map';
 
 export default class House extends React.Component {
-    render() {
-        return (
-            <div>
-                <Header />
-                <Map />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Map />
+      </div>
+    );
+  }
 }
